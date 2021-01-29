127bd54ec815bc92a14a9edce3e71b3ebb2febe5
ali Rashad almanea <37377385+Alialmanea@users.noreply.github.com>
2021-01-30 01:22:35 +0300

    Create IsPangram.java

---
+++ IsPangram.java
@@ -0,0 +1,9 @@
+public class PangramChecker {
+  public boolean check(String sentence){
+        for (char c = 'a'; c<='z'; c++)
+            if (!sentence.toLowerCase().contains("" + c))
+                return false;
+        return true;
+
+  }
+}
